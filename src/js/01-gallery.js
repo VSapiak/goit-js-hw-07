ee4423197ff82925dfb02e1702e781431ff14cb4
VSapiak <vsapiak5@gmail.com>
2023-04-03 17:16:09 +0100

    Finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
+
+const galleryItem = document.querySelector(".gallery");
+
+const imageItem = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+	 class="gallery__image"
+	 src="${preview}"
+	 data-source="${original}"
+	 alt="${description}"
+  />
+</a>
+</li>`
+  )
+  .join("");
+
+galleryItem.insertAdjacentHTML("beforeend", imageItem);
+galleryItem.addEventListener("click", (e) => {
+  e.preventDefault();
+  const target = e.target;
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  const galleryImageSrc = e.target.dataset.source;
+  const galleryAltText = e.target.alt;
+
+  const instance = basicLightbox.create(`
+    <img src="${galleryImageSrc}" alt="${galleryAltText}" width="800" height="600">
+`);
+
+  instance.show();
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+    return instance;
+  });
+});
 
 console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryItem = document.querySelector(".gallery");
+
+const imageItem = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+		<a class="gallery__link" href="${original}">
+			<img class="gallery__image" src="${preview}" alt="${description}" />
+		</a>
+	</li>`
+  )
+  .join("");
+
+galleryItem.insertAdjacentHTML("beforeend", imageItem);
+galleryItem.addEventListener("click", (e) => {
+  e.preventDefault();
+  const target = e.target;
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionsPosition: "bottom",
+    captionDelay: 250,
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      gallery.close();
+    }
+    return gallery;
+  });
+});
 
 console.log(galleryItems);
